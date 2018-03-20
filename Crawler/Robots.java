76d8232b7d4525bfff6a2e449aa5802a442cdcd5
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-03-20 16:36:41 +0200

    Still needs some fixing

---
+++ Crawler/Robots.java
@@ -0,0 +1,40 @@
+
+package crawler;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Robots 
+{   
+    ArrayList Disallowed_URLS;
+    
+    public void Parse_Robots(String url_tovisit) throws Exception
+    {
+        String url_robots = url_tovisit + "/robots.txt";
+        
+        try
+        {
+            URL url_ = new URL(url_robots);
+            Scanner scan = new Scanner(url_.openStream());
+            
+            while (scan.findInLine("User-agent: *") != null)
+            {
+                if (scan.findInLine("Disallow: /") != null)
+                {
+                   String str_exclude = scan.nextLine();
+                   System.out.print(str_exclude);
+                   Disallowed_URLS.add(str_exclude);
+                }
+            }
+            
+        }
+        catch(MalformedURLException e)
+        {
+            
+        }
+    }
+    
+}
