9d0acdcee51da6dbbc9c0b3b4798b6c7f886eef8
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-03-27 15:28:06 +0200

    hash set version

--- Crawler/Crawler.java
+++ Crawler/Crawler.java
@@ -1,6 +1,8 @@
-package crawler;
 
-import java.sql.*; 
+
+import java.sql.*;
+import java.net.*;  
+import java.util.Properties;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.HttpStatusException;
@@ -8,22 +10,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
-import java.net.SocketTimeoutException;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Crawler {
+public class crawler_v2 {
 
     private static final String DBCLASSNAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String CONNECTION =
-			"jdbc:sqlserver://localhost:1433;databaseName=;user=;password=;";
+			"jdbc:sqlserver://localhost:1433;databaseName=;user=sa;password=;";
    
    
     
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
  
-    System.out.println("From Crawler !");
+    System.out.println("From Crawler version 2 HashSets!");  
     String query_;
     Statement st2;
     ResultSet rt2;
+    
+    HashSet<String> crawled_sites = new HashSet<String>();
+    HashSet<String> status404_sites = new HashSet<String>();
     
     try{
         // DB Connection
@@ -31,23 +38,35 @@ public class Crawler {
         Connection con = DriverManager.getConnection(CONNECTION);
         System.out.println("Connected to database !");
   
+        // load any previous data first from the database 
+        // Note: Load only crawled sites, not the status404
+        String load_query = "SELECT URL from Docs_URL ORDER BY ID;";
+        Statement load_st = con.createStatement();
+        ResultSet load_result = load_st.executeQuery(load_query);
+        
+        while(load_result.next())
+        {
+            crawled_sites.add(load_result.getString(1));
+            System.out.println(load_result.getString(1));
+        }
+        
+        
         // Insert seed into database
-        String seed_ = "http://www.javatpoint.com/java-tutorial";
+        String seed_ = "https://www.javatpoint.com/java-tutorial";
         Document doc = Jsoup.connect(seed_).get();
         String title = doc.title();
         
-        try {
-            String check_id_seed = "Select ID from Docs_URL WHERE URL = '" + seed_ + "'" ;
-            Statement id_st_seed = con.createStatement();
-            ResultSet id_result_seed = id_st_seed.executeQuery(check_id_seed);
-            id_result_seed.next();
-            int temp_seed = id_result_seed.getInt(1);
-        
-        }
-        catch(SQLException sqle) 
+        Pattern pattseed = Pattern.compile("http(s)?://(www\\.)?");
+        Matcher matchseed = pattseed.matcher(seed_);
+        String resultseed = seed_;
+        if (matchseed.find())
         {
-            System.out.println("Sql Exception :"+sqle.getMessage());
-            String insert_seed_query = "Insert into Docs_URL (Title,URL) " + "Values ('" + trim(title,50) + "','" + seed_ + "')";
+           resultseed = seed_.replaceFirst(matchseed.group(0),"");
+        }
+        
+        if (crawled_sites.add(resultseed))
+        {
+            String insert_seed_query = "Insert into Docs_URL (Title,Protocol,URL) " + "Values ('" + trim(title,50) + "','" + matchseed.group(0) + "','" + seed_ + "')";
             Statement st = con.createStatement();
             st.executeUpdate(insert_seed_query);
 
@@ -60,39 +79,48 @@ public class Crawler {
             writer.print(doc);
             writer.close();
         }
-       
+        
         // printing links size  and imports - not important
         Elements links = doc.select("a[href]");
         System.out.println("imports = "+ links.size());
         print("\nLinks: (%d)", links.size());
         
-        
         // Inserting urls into hyperlinks table in ms sql database
-        // creating html docs for the fetched urls - should not be done here
-        // error: java.net.SocketTimeoutException: Read timed out
+        int count = 0;
+        
         for (Element link : links) 
         {
-            try
+            String filteredlink,protocol;
+            Pattern patt = Pattern.compile("http(s)?://(www\\.)?");
+            Matcher match = patt.matcher(link.attr("abs:href"));
+            if (match.find())
             {
-                String check_id = "Select ID from Docs_URL WHERE URL = '" + link.attr("abs:href") + "'" ;
-                Statement id_st = con.createStatement();
-                ResultSet id_result = id_st.executeQuery(check_id);
-                id_result.next();
-                int temp = id_result.getInt(1);
-                continue;
-                
-            }
-            catch(SQLException sqle) 
+                protocol = match.group(0);
+                filteredlink = link.attr("abs:href").replaceFirst(match.group(0),"");
+
+            if (!crawled_sites.contains(filteredlink) && !status404_sites.contains(filteredlink))
             {
-                //System.out.println("Sql Exception :"+sqle.getMessage());           
                 try
                 {
                     doc = Jsoup.connect(link.attr("abs:href")).get();
                     
                     title = doc.title();
                     //System.out.println(title);
+                    
+                    // check if it redirects to the same page
+                    patt = Pattern.compile("#");
+                    match = patt.matcher(link.attr("abs:href"));
+
+                    if (match.find())
+                    {
+                        count ++;
+                        System.out.println("# found count = " + count);
+                        continue;
+                    }
+
+                    crawled_sites.add(filteredlink);
                     print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-                    String query = "Insert into Docs_URL (Title,URL) " + "Values ('" + trim(title,50) + "','" +link.attr("abs:href")+ "')";
+                    String query = "Insert into Docs_URL (Title,Protocol,URL) " + "Values ('" + trim(title,50) + "','" + protocol + "','" + filteredlink+ "')";
                     Statement st_ = con.createStatement();
                     st_.executeUpdate(query);
 
@@ -105,22 +133,26 @@ public class Crawler {
                     writer_.print(doc);
                     writer_.close();
                     
-//                    if (rt2.getInt(1) == 65)
-//                        break;
+                    if (rt2.getInt(1) == 15)
+                    break;
+                    
                 }
                 catch (HttpStatusException http_e)
                 {
                     System.out.println("HTTP Status Exception:"+http_e.getMessage());
+                    status404_sites.add(filteredlink);
+
                 }
                 catch (SocketTimeoutException se)
                 {
                     System.out.println("Socket Timeout Exception:"+se.getMessage());
                 }
-
             }
-        
+        }
         }
 
+        System.out.println(crawled_sites.size());
+        System.out.println(status404_sites.size());
         System.out.println("It works !");
         con.close();
 
@@ -147,6 +179,5 @@ private static String trim(String s, int width) {
         else
                 return s;
 }
-
 
 }
