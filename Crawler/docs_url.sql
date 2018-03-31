e758944e518aa028b71204fc274908f19b357aa3
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-03-31 10:01:52 +0200

    Update docs_url.sql
    changed downloaded to visited
    increased url length to handle database exception of truncation

--- Crawler/docs_url.sql
+++ Crawler/docs_url.sql
@@ -1,7 +1,10 @@
 CREATE TABLE Docs_URL (
 ID int NOT NULL IDENTITY(1,1),
-Protocol varchar(25),
 Title varchar(50), 
-URL varchar(255) NOT NULL,
+URL varchar(2085) NOT NULL,
+Visited int,
 primary key(URL)
 );
+
+INSERT into Docs_URL (URL,Visited)
+VALUES ('https://www.theguardian.com/international',0);
