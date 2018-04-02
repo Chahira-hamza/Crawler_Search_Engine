cc6997ed64a9016b7eb6e09c367cb01e22920469
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-02 21:39:21 +0200

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -5,7 +5,7 @@ GO
 CREATE TABLE Docs_URL (
 ID int NOT NULL IDENTITY(1,1),
 Title varchar(50), 
-URL varchar(255) NOT NULL,
+URL varchar(2085) NOT NULL Unique,
 CONSTRAINT DOCS_ID UNIQUE(URL),
 primary key(ID)
 );
