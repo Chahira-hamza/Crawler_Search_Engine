2d885a7ae296878d6dfe0d24a70a0321c9d608a6
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-04-03 07:24:05 -0700

    Add files via upload

--- Integration/Integrator.java
+++ Integration/Integrator.java
@@ -13,7 +13,7 @@ public class Integrator {
     
     private static final String DBCLASSNAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String CONNECTION =
-			"jdbc:sqlserver://localhost:1433;databaseName=;user=;password=;";
+			"jdbc:sqlserver://localhost:1433;databaseName=search_engine;user=sa;password=gam3astuff*;";
     
     private static int threadNum;
     private static int depth;
@@ -25,33 +25,32 @@ public class Integrator {
     
         try
         {
-        con = connecttoDB();
-
-            // get input from user thread num, depth, max docs to crawl
-        
-            Scanner reader = new Scanner(System.in);  // Reading from System.in
-            System.out.println("Enter the desired number of threads: ");
-            threadNum = reader.nextInt(); // Scans the next token of the input as an int.
-
-            System.out.println("Enter the desired deph and the maximum document downloaded: ");
-            depth = reader.nextInt(); // Scans the next token of the input as an int.
-            maxDocs = reader.nextInt();
-
-            reader.close();
-        
-//
-//            threadNum = 2;
-//            depth = 1;
-//            maxDocs = 5;
+            con = connecttoDB();
+  
+            readInputfromUser();
+            
+//          threadNum = 2;
+//          depth = 1;
+//          maxDocs = 5;
             lock = new Object();
 
             Thread CrawlerManag = new Thread(new CrawlerManager(depth,threadNum,maxDocs,con,lock));
             Thread IndexerThread = new Thread(new Indexer(con,lock));
+            
             CrawlerManag.start();
             IndexerThread.start();
-            IndexerThread.join();
+           
             CrawlerManag.join();
-
+            
+            synchronized(lock)
+            {
+                IndexerThread.interrupt();
+                lock.notify();
+            }
+            
+            
+            IndexerThread.join();
+            
         System.out.println("Closing connection!!!");
         con.close();
         }
@@ -60,6 +59,20 @@ public class Integrator {
          System.out.println(e.getMessage());
     }
     
+}
+    
+private static void readInputfromUser()
+{
+    // get input from user thread num, depth, max docs to crawl
+    Scanner reader = new Scanner(System.in);  // Reading from System.in
+    System.out.println("Enter the desired number of threads: ");
+    threadNum = reader.nextInt(); // Scans the next token of the input as an int.
+
+    System.out.println("Enter the desired deph and the maximum documents to download: ");
+    depth = reader.nextInt(); // Scans the next token of the input as an int.
+    maxDocs = reader.nextInt();
+
+    reader.close();
 }
     
     //Establish connection with the Database   
