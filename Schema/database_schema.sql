6d3d2f157a2805c58e7b9b8d68fabda807f7811e
Chahira-hamza <33982917+Chahira-hamza@users.noreply.github.com>
2018-05-01 14:57:16 -0700

    Add files via upload
    updated schema with linkRank, pageRank, rankedBit

--- Schema/database_schema.sql
+++ Schema/database_schema.sql
@@ -1,4 +1,6 @@
 
+use search_engine;
+
 CREATE TABLE Docs_URL (
 ID int NOT NULL IDENTITY(1,1),
 Title varchar(50), 
@@ -6,11 +8,10 @@ URL varchar(2085) NOT NULL UNIQUE,
 CONSTRAINT DOCS_ID UNIQUE(URL),
 Visited int,
 linkRank int,
+pageRank float,
+rankedBit bit,
 primary key(ID)
 );
-
-INSERT into Docs_URL (URL,Visited)
-VALUES ('https://www.theguardian.com',0);
 
 CREATE TABLE Words
 (   Doc_ID int NOT NULL,
@@ -23,7 +24,6 @@ CONSTRAINT FK_worddoc FOREIGN KEY (Doc_ID) REFERENCES Docs_URL(ID)
     ON DELETE CASCADE    
     ON UPDATE CASCADE    
 );  
-
 
 	CREATE TABLE WordPostions
 (   Doc_ID int NOT NULL,
@@ -46,3 +46,52 @@ CONSTRAINT FK_TextDoc FOREIGN KEY (Doc_ID)
     ON DELETE CASCADE    
     ON UPDATE CASCADE
 	);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.theguardian.com',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('http://www.bbc.com/news',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('http://www.bbc.com/news/world',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('http://www.bbc.com/news/business',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.bbc.com/sport',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.bbc.com/weather',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.bbc.co.uk/music',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.bbc.com/food',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.theguardian.com/us/sport',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.theguardian.com/us-news/us-politics',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://www.theguardian.com/world',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://edition.cnn.com',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://edition.cnn.com/politics',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://edition.cnn.com/sport',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://edition.cnn.com/health',0,0,1,0);
+
+INSERT into Docs_URL (URL,Visited,linkRank,pageRank, rankedBit)
+VALUES ('https://en.wikipedia.org/wiki/Main_Page',0,0,1,0);
+
